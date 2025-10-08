99886fff2609e62ae590c12afc9251006f8b65ce
ikrambenallali <ikrambenallali2003@gmail.com>
2025-10-08 11:55:53 +0100

    EM-13 delete user

--- controller/UserController.js
+++ controller/UserController.js
@@ -31,4 +31,17 @@ async function getAllUsers(req, res) {
         res.status(500).json({ error: 'Failed to fetch users' });
     }
 }
-module.exports = { createUser, getAllUsers };
+async function deleteUser(req, res) {
+    const { id } = req.params;
+   try {
+         const user = await User.findByIdAndDelete(id);
+         if (!user) {
+             return res.status(404).json({ error: 'User not found' });
+         }
+         res.status(200).json({ message: 'User deleted successfully' });
+   } catch (error) {
+       console.error('Error deleting user:', error);
+       res.status(500).json({ error: 'Failed to delete user' });
+   }
+}
+module.exports = { createUser, getAllUsers, deleteUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser ,getAllUsers } = require('../controller/UserController');
+const { createUser ,getAllUsers ,deleteUser } = require('../controller/UserController');
 const validationMiddleware = require("../middlewares/validation");
 const {userSchema} = require("../middlewares/schema");
 const validator = new validationMiddleware();
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.post('/createUser',validator.validate(userSchema), createUser);
 router.get('/getAllUsers', getAllUsers);
+router.delete('/deleteUser/:id', deleteUser);
 
 module.exports = router;
