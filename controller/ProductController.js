112389fb4085e36e9a38912459b072eb582ce9b5
ikrambenallali <ikrambenallali2003@gmail.com>
2025-10-09 11:11:46 +0100

    EM-6 get product by id

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -46,4 +46,20 @@ async function restoreProduct(req, res) {
         res.status(500).json({ error: 'Failed to fetch products' });
     }
 }
-module.exports = { createProduct, getAllProducts, deleteProduct , restoreProduct};
+
+//get products by id 
+async function getProductById(req, res) {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product || product.isDeleted) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).json({ error: 'Failed to fetch product' });
+    }
+}
+
+module.exports = { createProduct, getAllProducts, deleteProduct , restoreProduct, getProductById };
